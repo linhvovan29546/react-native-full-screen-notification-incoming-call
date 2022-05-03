7bb1eda7fa45868f03883a4337207355924e7433
Linh Võ <linh.vo@hdwebsoft.co>
2022-05-03 09:26:26 +0700

    create nodemodule for api show/hide notification

--- android/src/main/java/com/reactnativefullscreennotificationincomingcall/FullScreenNotificationIncomingCallModule.java
+++ android/src/main/java/com/reactnativefullscreennotificationincomingcall/FullScreenNotificationIncomingCallModule.java
@@ -1,19 +1,24 @@
 package com.reactnativefullscreennotificationincomingcall;
 
+import android.content.Intent;
+
 import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 @ReactModule(name = FullScreenNotificationIncomingCallModule.NAME)
 public class FullScreenNotificationIncomingCallModule extends ReactContextBaseJavaModule {
     public static final String NAME = "FullScreenNotificationIncomingCall";
-
-    public FullScreenNotificationIncomingCallModule(ReactApplicationContext reactContext) {
-        super(reactContext);
+    public static ReactApplicationContext reactContext;
+    public FullScreenNotificationIncomingCallModule(ReactApplicationContext context) {
+        super(context);
+        reactContext = context;
     }
 
     @Override
@@ -21,6 +26,37 @@ public class FullScreenNotificationIncomingCallModule extends ReactContextBaseJa
     public String getName() {
         return NAME;
     }
+
+    @ReactMethod
+    public void displayNotification(String uuid, String name, String avatar, String info,String channelId,String channelName, int timeout) {
+      Intent intent = new Intent(getReactApplicationContext(), IncomingCallService.class);
+      intent.putExtra("uuid", uuid);
+      intent.putExtra("name",name );
+      intent.putExtra("avatar", avatar);
+      intent.putExtra("info", info);
+      intent.putExtra("channelId", channelId);
+      intent.putExtra("channelName", channelName);
+      intent.putExtra("timeout", timeout);
+      intent.setAction(Constants.ACTION_SHOW_INCOMING_CALL);
+      getReactApplicationContext().startService(intent);
+    }
+
+    @ReactMethod
+    public void hideNotification() {
+//      if (UnlockScreenActivity.active) {
+//        UnlockScreenActivity.getInstance().destroyActivity(false);
+//      }
+       Intent intent = new Intent(getReactApplicationContext(), IncomingCallService.class);
+       intent.setAction(Constants.HIDE_NOTIFICATION_INCOMING_CALL);
+       getReactApplicationContext().stopService(intent);
+    }
+
+    @ReactMethod
+    public static void sendEventToJs(String eventName, WritableMap params) {
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName,params);
+    }
+
+
 
 
     // Example method
